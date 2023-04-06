601a8b0c51af328f3e380cb9fe421fcdd10b8d8c
RenanGsilvaa <profissionalrenann@outlook.com>
2023-04-05 22:36:48 -0300

    Criando meu aplicativo, onde eu vejo ele pelo menos

--- App.js
+++ App.js
@@ -4,7 +4,18 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.texto}>Eu sou o Renan e esse é o meu primeiro projeto em Js</Text>
+      <Text style={styles.texto}>Eu espero gostar bastante desse conteudo</Text>
+      <Text style={styles.socorro}>Agora um pouco de depressão</Text>
+      <Text style={styles.socorro}>Meu Deus meu senhor, me ajudaa, por favor!</Text>
+      <Text style={styles.linha}>--------------------------------------------------------</Text>
+      <Text style={styles.texto}>Hello, i´m Renan and this is my first project in Js</Text>
+      <Text style={styles.texto}>I hope like this content</Text>
+      <Text style={styles.socorro}>Now some kind of depression</Text>
+      <Text style={styles.socorro}>My God, my Lord, help me, please</Text>
+      <Text style={styles.linha}>--------------------------------------------------------</Text>
+      <Text style={styles.socorro}>Socoroooooooooooo</Text>
+      <Text style={styles.texto}>Helpppppppppppppp</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +24,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
+    flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'start',
   },
+  texto: {
+    backgroundColor: "#FF69B4",
+    padding: 10,
+    borderRadius: 5,
+    margin: 5
+  },
+  socorro: {
+    backgroundColor: '#12FF08',
+    padding: 10,
+    borderRadius: 5,
+    margin: 5
+  },
+  linha: {
+    color: '#FFF',
+  }
 });
