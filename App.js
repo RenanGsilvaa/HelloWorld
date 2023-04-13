b4c91d4a599516ab77cb85ce6c6d0807f6a52f6b
Renan <profissionalrenann@outlook.com>
2023-04-12 22:12:03 -0300

    Criando a Aplicação

--- App.js
+++ App.js
@@ -1,47 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+//Paginas
+import Home from '/.src/pages/Home';
+import Formacao from './src/pages/Formacao';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.texto}>Eu sou o Renan e esse é o meu primeiro projeto em Js</Text>
-      <Text style={styles.texto}>Eu espero gostar bastante desse conteudo</Text>
-      <Text style={styles.socorro}>Agora um pouco de depressão</Text>
-      <Text style={styles.socorro}>Meu Deus meu senhor, me ajudaa, por favor!</Text>
-      <Text style={styles.linha}>--------------------------------------------------------</Text>
-      <Text style={styles.texto}>Hello, i´m Renan and this is my first project in Js</Text>
-      <Text style={styles.texto}>I hope like this content</Text>
-      <Text style={styles.socorro}>Now some kind of depression</Text>
-      <Text style={styles.socorro}>My God, my Lord, help me, please</Text>
-      <Text style={styles.linha}>--------------------------------------------------------</Text>
-      <Text style={styles.socorro}>Socoroooooooooooo</Text>
-      <Text style={styles.texto}>Helpppppppppppppp</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home}/>
+        <Stack.Screen name="Formacao" component={Formacao}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'start',
-  },
-  texto: {
-    backgroundColor: "#FF69B4",
-    padding: 10,
-    borderRadius: 5,
-    margin: 5
-  },
-  socorro: {
-    backgroundColor: '#12FF08',
-    padding: 10,
-    borderRadius: 5,
-    margin: 5
-  },
-  linha: {
-    color: '#FFF',
-  }
-});
---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Página Inicial</Text>
+            <Text>Por que não quer aparecer</Text>
+        </View>
+    );
+}
+
+const estilos = StyleSheet.create({
+
+});
+
+export default Home;
